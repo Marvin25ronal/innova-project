f11af7b4f78914c49d8248d32de7662050350201
Marvin Martínez <marvin1ronal@gmail.com>
2024-05-07 22:57:20 -0600

    :memo: swagger

---
+++ InnovaProject/src/main/java/com/example/innovaproject/config/OpenApiConfig.java
@@ -0,0 +1,12 @@
+package com.example.innovaproject.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(info = @Info(title = "Innova Project API", version = "1.0", description = "Innova Project API Information"))
+public class OpenApiConfig {
+
+
+}
